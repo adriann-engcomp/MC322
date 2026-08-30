a8ec7a67e2185d3faa1bee90a7009df6471e5692
Adriann <a267336@dac.unicamp.br>
2026-08-29 22:25:22 -0300

    Arquivos que tentei implementar

--- Inicio/Ex1.java
+++
@@ -1,3 +0,0 @@
-public static void imprimirAltura(double altura) {
-    System.out.println(altura + " metros");
-}
---
+++ src/MateriaPrima.java
@@ -0,0 +1,11 @@
+public class MateriaPrima {
+    // 1. Atributos (o que é?)
+    private int Id;
+    private String Nome;
+    private double Quantidade;
+    private String Unidade;
+    private double QuantidadeMinima;
+
+    // 2. Metodos (o que faz?)
+    public int consumir(int QuantidadeMinima)
+}
