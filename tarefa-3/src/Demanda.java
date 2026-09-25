1019742b6db625b117bf891e96f854644ba58bf2
Adriann <a267336@dac.unicamp.br>
2026-09-25 17:40:17 -0300

    Acredito que terminei a demanda (vai para a revisão) e salvei o comando que comenta um bloco de codigo

--- tarefa-3/src/Demanda.java
+++ tarefa-3/src/Demanda.java
@@ -1,56 +1,122 @@
 public class Demanda {
+    // Novos tributos
+    private static int proximoNumero = 1;
+    // Ordem de chegada da demanda usando a fila FIFO
+    private final int numero;
+    private final TipoMedicamento tipo;
+    private final String medicamento;
+    private final double custoOperacionalPorUnidade;
+    private int quantidade;
+    private StatusDemanda status;
 
-    // Atributos
-    private String tipoProduto;
-    private int quantidadeProdutos;
-    private boolean atendida;
-
-    // Construtor
-    public Demanda(String tipoProduto, int quantidadeProdutos) {
-        this.tipoProduto = tipoProduto;
-        this.quantidadeProdutos = quantidadeProdutos;
-        this.atendida = false;
+    public Demanda(TipoMedicamento tipo, String medicamento, int quantidade, double custoOperacionalPorUnidade){
+        if (tipo == null || quantidade < 0){
+            throw new IllegalArgumentException("É obrigatório definir o tipo do medicamento e a quantidade não pode ser negativa");
+        }
+        this.numero = proximoNumero++;
+        this.tipo = tipo;
+        this.medicamento = medicamento;
+        this.quantidade = quantidade;
+        this.custoOperacionalPorUnidade = custoOperacionalPorUnidade;
+        this.status = StatusDemanda.PENDENTE;
     }
+
 
     // Métodos
-    public void atualizarQuantidade(int novaQuantidade) {
+    /* public void atender() {
+        this.atendida = true;
+    } */
+    private void mudarStatus(StatusDemanda novo){
+        if (!status.mudaPara(novo)){
+            throw new IllegalArgumentException(String.format(
+                "Transição inválida no seu pedido #%d: %s -> %s",
+            numero, status.getDescricao(), novo.getDescricao()));
+        }
+        this.status = novo;
+    }
+    public void iniciarProducao(){
+        mudarStatus(StatusDemanda.EM_PRODUCAO);
+    }
+    public void concluir(){
+        mudarStatus(StatusDemanda.CONCLUIDA);
+    }
+    public void cancelar(){
+        mudarStatus(StatusDemanda.CANCELADA);
+    }
+    /* public void atualizarQuantidade(int novaQuantidade) {
         this.quantidadeProdutos = novaQuantidade;
         this.atendida = false;
+    }, se torna:
+    */
+    // Trato primeiro o caso de quem recebe uma nova quantidade (PENDENTES ou CONCLUIDOS)
+    public boolean podeSerAtualizada(){
+        return status == StatusDemanda.PENDENTE || status == StatusDemanda.CONCLUIDA;
     }
 
-    public double calcularMateriaPrimaNecessaria(double materiaPrimaPorUnidade) {
+    public void atualizarQuantidade(int novaQuantidade){
+        // No caso de está PENDENTE atribuo uma nova demanda
+        if (novaQuantidade < 0){
+            throw new IllegalArgumentException("A quantidade nunca é negativa");
+        }
+        if (!podeSerAtualizada()){
+            throw new IllegalStateException("Pedido #" + numero + "está " + status.getDescricao() + " e não pode ser alterado.");
+        }
+        this.quantidade = novaQuantidade;
+        if (status == StatusDemanda.CONCLUIDA){
+            mudarStatus(StatusDemanda.PENDENTE);
+        }
+    }
+
+    /* public double calcularMateriaPrimaNecessaria(double materiaPrimaPorUnidade) {
         return materiaPrimaPorUnidade * quantidadeProdutos;
+    } */
+    // ??
+    public boolean estaElegivel(){
+        return status == StatusDemanda.PENDENTE && quantidade > 0;
+    }
+    // kg/demanda (quantidade)
+    public double calcularConsumoPorDemanda(){
+        return quantidade * tipo.getConsumoPorUnidade();
+    }
+    // R$/demanda (quantidade)
+    public double calcularCustoPorDemanda(){
+        return quantidade * custoOperacionalPorUnidade;
+    }
+    // Consigo produzir?
+    public boolean cabeNoOrcamento(double orcamentoDisponivel){
+        return calcularCustoPorDemanda() <= orcamentoDisponivel;
     }
 
-    public void atender() {
-        this.atendida = true;
-    }
 
     // Getters e Setters
-    // TipoProduto
-    public String getTipoProduto() {
-        return tipoProduto;
+    public int getNumero(){
+        return numero;
+    }
+    
+    public TipoMedicamento getTipo(){
+        return tipo;
     }
 
-    public void setTipoProduto(String tipoProduto) {
-        this.tipoProduto = tipoProduto;
+    public String getMedicamento(){
+        return medicamento;
     }
 
-    // QuantidadeProdutos
-    public int getQuantidadeProdutos() {
-        return quantidadeProdutos;
+    public int getQuantidade(){
+        return quantidade;
     }
 
-    public void setQuantidadeProdutos(int quantidadeProdutos) {
-        this.quantidadeProdutos = quantidadeProdutos;
+    public StatusDemanda getStatus(){
+        return status;
     }
 
-    // Atendida
-    public boolean isAtendida() {
-        return atendida;
+    public double getCustoOperacionalPorUnidade(){
+        return custoOperacionalPorUnidade;
     }
 
-    public void setAtendida(boolean atendida) {
-        this.atendida = atendida;
+    @Override
+    public String toString(){
+        return String.format("Pedido #%d - %s (%s) x%d [%s]",
+            numero, medicamento, tipo.getNome(), quantidade, status.getDescricao());
     }
+
 }
